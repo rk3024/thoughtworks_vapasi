de9ad1b9994b5f2cf81f2830df9021c2394997a2
ravinderkabil <kabli.ravinder@gmail.com>
2025-06-30 17:24:27 +0530

    Committed assignments for Custom Exception Handling

--- Java_Projects/src/exceptionHandling/MyCalcApp.java
+++ Java_Projects/src/exceptionHandling/MyCalcApp.java
@@ -6,10 +6,15 @@ package exceptionHandling;
 public class MyCalcApp {
     public static void main(String[] args) {
         if(args != null && args.length>0){
-            int num = Integer.parseInt(args[0]);
-            Calculator calculator = new Calculator();
-            double doubleValue = calculator.calDouble(num);
-            System.out.println("Double value returned is " + doubleValue);
+            try{
+                int num = Integer.parseInt(args[0]);
+                Calculator calculator = new Calculator();
+                double doubleValue = calculator.calDouble(num);
+                System.out.println("Double value returned is " + doubleValue);
+            }catch(MyArithException maex){
+                System.out.println("Number less than or equal to zero encountered" + maex.getMessage());
+            }
+
         }
     }
 }
